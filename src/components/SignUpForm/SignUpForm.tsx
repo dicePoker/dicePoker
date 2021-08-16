57e2aa903947017208c0d474f6a2cd6e4286909e
Kirill Gromov <kvgromov@mts.ru>
2021-08-16 22:17:31 +0300

    Код ревью

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import {FormControl, Button, TextField, Link} from "@material-ui/core";
-import './signUpForm.scss'
+import './SignUpForm.scss'
 
 const useStyles = makeStyles((theme) => ({
   link: {
@@ -17,18 +17,18 @@ const useStyles = makeStyles((theme) => ({
 
 }));
 
-export default function signUpForm() {
+export default function SignUpForm(): JSX.Element {
   const classes = useStyles();
   return (
     <div className="sign-up-form">
       <h1 className="sign-up-form__title">Регистрация</h1>
       <FormControl component="form">
-        <TextField  type="text"  label="Имя"/>
-        <TextField  type="text"  label="Фамилия"/>
-        <TextField  type="email"  label="E-mail"/>
-        <TextField  type="tel"  label="Телефон"/>
-        <TextField  type="text"  label="Логин"/>
-        <TextField  type="password"  label="Пароль"/>
+        <TextField name="first_name" type="text"  label="Имя"/>
+        <TextField name="last_name" type="text"  label="Фамилия"/>
+        <TextField name="email" type="email"  label="E-mail"/>
+        <TextField name="phone" type="tel"  label="Телефон"/>
+        <TextField name="login" type="text"  label="Логин"/>
+        <TextField name="password" type="password"  label="Пароль"/>
         <Button variant="contained" size="medium" color="primary" className={classes.button} >Регистрация</Button>
       </FormControl>
       <Link href="#" className={classes.link} onClick={(e)=> e.preventDefault()}>
