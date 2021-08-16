bc10116526c07ad3841d3a3f61e1e3cf3e99b000
Kirill Gromov <kvgromov@mts.ru>
2021-08-16 09:15:39 +0300

    Страница авторизации

---
+++ src/pages/signIn/signIn.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import SignInForm from "../../components/signInForm/signInForm";
+
+export default function SignIn() {
+  return (
+    <div className="sign-in">
+      <SignInForm/>
+    </div>
+  )
+}
