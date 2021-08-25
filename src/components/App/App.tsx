1001dc901e5af1dbd441ce613f4119f41a0b3509
Angry Fennec <shellfix@gmail.com>
2021-08-25 19:57:31 +0300

    Добавляет канвас

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './App.scss';
+import { Main } from '../Main/Main';
 
 export const App = (): JSX.Element => (
   <div className="App">
     <h1>Test App</h1>
+    <Main />
   </div>
 );
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,8 +1,52 @@
 import React from 'react';
+import './Main.scss';
 import { useStyles } from '../../utils/makeStyles';
+import { Button, Grid } from '@material-ui/core';
 
 export const Main = (): JSX.Element => {
   const classes = useStyles();
 
-  return <section className="main"></section>;
+  return (
+    <section className="main">
+      <Grid
+        container
+        direction="column"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <canvas id="canvas" className="main__canvas" />
+        <ul className="main__btn-list">
+          <li className="main__btn-item">
+            <Button
+              variant="contained"
+              size="medium"
+              color="primary"
+              className={classes.button}
+            >
+              Перебросить
+            </Button>
+          </li>
+          <li className="main__btn-item">
+            <Button
+              variant="contained"
+              size="medium"
+              color="secondary"
+              className={classes.button}
+            >
+              Завершить
+            </Button>
+          </li>
+          <li className="main__btn-item">
+            <Button
+              variant="contained"
+              size="medium"
+              className={classes.button}
+            >
+              Таблица
+            </Button>
+          </li>
+        </ul>
+      </Grid>
+    </section>
+  );
 };
