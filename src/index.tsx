496f00e90cb90ad2ca95ce0283ee98a52e56aaee
Kirill Gromov <kvgromov@mts.ru>
2021-09-01 10:21:37 +0300

    Правки

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,23 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { App } from './components/App/App';
 
-debugger;
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', async () => {
+      try {
+        await navigator.serviceWorker.register('/sw.js');
+      } catch (error) {
+        throw new Error(error);
+      }
+    });
+  }
+}
+
+startServiceWorker();
+
 ReactDOM.render(
   <BrowserRouter basename="/">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
-
-function startServiceWorker() {
-  if ('serviceWorker' in navigator) {
-    window.addEventListener('load', () => {
-      navigator.serviceWorker
-        .register('/sw.ts')
-        .then(registration => {
-          console.log(
-            'ServiceWorker registration successful with scope: ',
-            registration.scope,
-          );
-        })
-        .catch((error: string) => {
-          console.log('ServiceWorker registration failed: ', error);
-        });
-    });
-  }
-}
-
-// startServiceWorker();
