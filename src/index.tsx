894799043b968f81986db5f5e4b07271ff6dcce4
Kirill Gromov <kvgromov@mts.ru>
2021-09-01 10:20:17 +0300

    Добавил офлайн режим через (WorkBox)

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,30 @@ import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import { App } from './components/App/App';
 
+debugger;
 ReactDOM.render(
   <BrowserRouter basename="/">
     <App />
   </BrowserRouter>,
   document.getElementById('root'),
 );
+
+function startServiceWorker() {
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/sw.ts')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with scope: ',
+            registration.scope,
+          );
+        })
+        .catch((error: string) => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
+    });
+  }
+}
+
+// startServiceWorker();
