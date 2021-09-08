f0da69704cfac34abfb4dacd245b28e6c908263b
Kirill Gromov <kvgromov@mts.ru>
2021-09-08 21:46:06 +0300

    Компонент GameOver

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -7,11 +7,18 @@ import { SignIn } from '../../pages/SignIn/SignIn';
 import { SignUp } from '../../pages/SignUp/SignUp';
 import { Dashboard } from '../../pages/Dashboard/Dashboard';
 import { Forum } from '../../pages/Forum/Forum';
+import { GameOver } from '../GameOver/GameOver';
 
 export const App = (): JSX.Element => (
   <ErrorBoundary>
     <div className="App">
       <MaterialDemo />
+      <GameOver
+        title={'игрок 1'}
+        handlerClick={() => {
+          console.log('click');
+        }}
+      />
       <Switch>
         <Route path="/signin/" component={SignIn} />
         <Route path="/signup/" component={SignUp} />
---
+++ src/components/GameOver/GameOver.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './GameOver.scss';
+
+import ChestImage from 'src/static/assets/img/chest.png';
+
+type GameOverType = {
+  title: string;
+  handlerClick(): void;
+};
+
+export const GameOver = (props: GameOverType): JSX.Element => {
+  return (
+    <div className="game-over">
+      <h2 className="game-over__title">Победил !</h2>
+      <h3 className="game-over__name">{props.title}</h3>
+      <div className="game-over__wrapper">
+        <img src={ChestImage} className="game-over__img" />
+      </div>
+      <button className="game-over__button" onClick={props.handlerClick}>
+        Новый вызов !
+      </button>
+    </div>
+  );
+};
