b76df15eae4f26a870af456983444c2026c7ab77
mankov <mankov@farzoom.com>
2021-09-10 01:09:41 +0700

    настроен api регистрации и смены профиля

--- src/redux/actions/actions.ts
+++ src/redux/actions/actions.ts
@@ -3,6 +3,8 @@ import {
   ActionloadingFailed,
   ActionloadingSuccess,
   ActionResetLoading,
+  ActionSetIsAuth,
+  ActionSetUserInfo,
   ActionSetUsers,
 } from './types';
 import { ThunkAction } from 'redux-thunk';
@@ -41,6 +43,94 @@ const setUsers = (users: StateTypes['users']): ActionSetUsers => {
     type: ActionTypes.SET_USERS,
     payload: users,
   };
+};
+
+const setUserInfo = (userInfo: StateTypes['userInfo']): ActionSetUserInfo => {
+  return {
+    type: ActionTypes.SET_USER_INFO,
+    payload: userInfo,
+  };
+};
+
+const setIsAuth = (isAuth: boolean): ActionSetIsAuth => {
+  return {
+    type: ActionTypes.SET_IS_AUTH,
+    payload: isAuth,
+  };
+};
+
+export const createNewUser =
+  (data: Omit<StateTypes['userInfo'], 'id'>): ThunkType =>
+  (dispatch, getState, apiService) => {
+    dispatch(loadingSuccess());
+    apiService
+      .createNewUser(data)
+      .then((response: { id: number }) => {
+        const userInfo = { id: response?.id, ...data };
+        dispatch(setUserInfo(userInfo));
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => dispatch(loadingFailed()));
+  };
+
+export const changeProfileData =
+  (data: StateTypes['userInfo'] & { display_name: string }): ThunkType =>
+  (dispatch, getState, apiService) => {
+    dispatch(loadingSuccess());
+    apiService
+      .changeProfileData({ ...data, display_name: data.login })
+      .then((response: StateTypes['userInfo']) => {
+        dispatch(setUserInfo(response));
+      })
+      .catch(error => {
+        console.log(error);
+      })
+      .finally(() => dispatch(loadingFailed()));
+  };
+
+export const authorization =
+  (data: { login: string; password: string }): ThunkType =>
+  (dispatch, getState, apiService) => {
+    dispatch(loadingSuccess());
+    apiService
+      .authorization(data)
+      .then((response: string) => {
+        dispatch(setIsAuth(true));
+        dispatch(getUser());
+      })
+      .catch(error => {
+        dispatch(setIsAuth(false));
+        console.log(error);
+      })
+      .finally(() => dispatch(loadingFailed()));
+  };
+
+export const logout = (): ThunkType => (dispatch, getState, apiService) => {
+  dispatch(loadingSuccess());
+  apiService
+    .logout()
+    .then((response: string) => {
+      dispatch(setIsAuth(false));
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => dispatch(loadingFailed()));
+};
+
+export const getUser = (): ThunkType => (dispatch, getState, apiService) => {
+  dispatch(loadingSuccess());
+  apiService
+    .getUser()
+    .then((response: StateTypes['userInfo']) => {
+      dispatch(setUserInfo(response));
+    })
+    .catch(error => {
+      console.log(error);
+    })
+    .finally(() => dispatch(loadingFailed()));
 };
 
 export const fetchUsers = (): ThunkType => (dispatch, getState, apiService) => {
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -3,16 +3,27 @@ import {
   ActionloadingSuccess,
   ActionResetLoading,
   ActionSetUsers,
+  ActionSetUserInfo,
+  ActionSetIsAuth,
 } from './actions/types';
 
 export interface StateTypes {
   loading: number;
+  isAuth: boolean;
   users: {
     id: string;
     name: string;
     rating: string;
     record: string;
   }[];
+  userInfo: {
+    id: number | null;
+    first_name: string | null;
+    second_name: string | null;
+    login: string | null;
+    email: string | null;
+    phone: string | null;
+  };
 }
 
 export enum ActionTypes {
@@ -20,10 +31,14 @@ export enum ActionTypes {
   LOADING = 'LOADING',
   LOADING_OFF = 'LOADING_OFF',
   SET_USERS = 'SET_USERS',
+  SET_USER_INFO = 'SET_USER_INFO',
+  SET_IS_AUTH = 'SET_IS_AUTH',
 }
 
 export type ActionType =
   | ActionResetLoading
   | ActionloadingSuccess
   | ActionloadingFailed
-  | ActionSetUsers;
+  | ActionSetUsers
+  | ActionSetUserInfo
+  | ActionSetIsAuth;
