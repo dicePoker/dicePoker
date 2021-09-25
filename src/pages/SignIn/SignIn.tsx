266b88b20e426eb35ed50ce5aa21749d0eec5a6c
Kirill Gromov <kvgromov@mts.ru>
2021-09-25 21:43:30 +0300

    Удалил лишнее

--- src/pages/SignIn/SignIn.tsx
+++ src/pages/SignIn/SignIn.tsx
@@ -1,7 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { SignInForm } from '@/components/SignInForm/SignInForm';
-import { Modal } from '@/components/Modal/Modal';
-import { Pause } from '@/components/Pause/Pause';
 
 export const SignIn = (): JSX.Element => {
   return (
